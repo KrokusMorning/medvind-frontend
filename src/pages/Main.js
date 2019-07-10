915644d45996ade932e01cf5b45e4b49c3a4c62b
Ossian Fjordefalk <ocean@ossians-mbp-2.mynet>
2019-07-10 10:38:34 +0200

    initiate project

---
+++ src/pages/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+/**
+ * The Main page.
+ */
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to Medvind!</h1>
+            </div>
+        );
+    }
+}
+
+export default Main;
